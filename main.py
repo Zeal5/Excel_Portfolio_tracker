ddbc2141836ddf2bb6274667ab214a5c9a9fa65d
Zeal5 <tassadiqshakeel5@gmail.com>
2024-01-05 12:49:25 +0500

    write output in seperate csv file complete

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class TickerData:
         """get old avg.price add new price and divide by total amount"""
         self.holdings += _amount
         self.total_cost += price * _amount
-        print(f"amount {self.holdings}\ttotal cost : {self.total_cost}")
+        # print(f"amount {self.holdings}\ttotal cost : {self.total_cost}")
         self.avg_buy_price = self.total_cost / self.holdings
 
     def calc_break_even_price(self, _price: float, _amount_sold: float):
@@ -34,7 +34,7 @@ class TickerData:
         self.breakeven_price = self.total_cost / self.holdings
 
 
-def calculate_average(_ticker: str, _df: pd.DataFrame):
+def calculate_average(_ticker: str, _df: pd.DataFrame) -> dict:
     """Calculate average price for each ticker Df from all the entries Df"""
     ticker = TickerData(_ticker)
 
@@ -58,16 +58,15 @@ def calculate_average(_ticker: str, _df: pd.DataFrame):
         "total_cost": ticker.total_cost,
         "total_profit": ticker.total_profit,
     }
-    pprint(summary)
+    return summary
 
 
 def load_excel(_sheet_name: str):
     xl = pd.ExcelFile(_sheet_name)
     df = xl.parse("Sheet1")
     df.columns = df.columns.str.lower()
-    df = df.applymap(lambda x: x.lower() if type(x) == str else x)
+    df = df.map(lambda x: x.strip().lower() if type(x) == str else x)
     df = df.dropna()
-    print("-" * 50)
     print(df)
     print("-" * 50)
     return df
@@ -75,8 +74,8 @@ def load_excel(_sheet_name: str):
 
 def get_unique_ticker(_df: pd.DataFrame) -> set:
     """Return unique tickers from dataframe"""
-    tickers = {i.lower() for i in _df["ticker"].tolist()}
-    # print(tickers)
+    tickers = {i.lower().strip() for i in _df["ticker"].tolist()}
+    print(tickers)
     return tickers
 
 
@@ -84,12 +83,25 @@ def main():
     sheet_name = "Book1.xlsx"
     df = load_excel(sheet_name)
     unique_tickers = get_unique_ticker(df)
+    summary_dict = {}
     for ticker in unique_tickers:
         """Seperate rows for each ticker into df"""
         # print(ticker)
         df_ticker = df.loc[df["ticker"] == ticker]
         # print(df_ticker)
-        calculate_average(ticker, df_ticker)
+        summary_dict[ticker] = calculate_average(ticker, df_ticker)
+
+    # pprint(summary_dict, indent =3)
+    new_df = pd.DataFrame(summary_dict)
+    new_df = new_df.transpose()
+    # new_df.drop(new_df.index[0], inplace=True)
+    # print(new_df)
+    # # new_df.to_excel("Book2.xlsx", sheet_name = "summary")
+    print(new_df)
+    with pd.ExcelWriter('Book2.xlsx', mode='a',engine="openpyxl", if_sheet_exists="overlay") as writer:
+       new_df.to_excel(writer, sheet_name='summary',header= False,index=False, startrow= 1)
+    print('done')
+
 
 
 main()
